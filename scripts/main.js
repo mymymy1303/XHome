9e0cabdc855e1be348e6395c216586f6e91299aa
mymymy1303 <thehuy160897@gmail.com>
2018-11-25 02:50:30 +0800

    Done gioi thieu

--- scripts/main.js
+++ scripts/main.js
@@ -119,51 +119,89 @@ const theHuyApp = {
 }
 
 theHuyApp.ready(() => {
+	/////////////-----------GLOBAL------------///////////////
+	
 	//Initialize AOS plugin
 	theHuyApp.avoidNull(aosInit);
 	theHuyApp.avoidNull(aosRefreshAfterLoad);
+	
+	//Back to top button
+	theHuyApp.avoidNull(clickBackToTop);
+	
+	//Set breadcrumb home icon
+	theHuyApp.avoidNull(setBreadcrumbHomeIcon);
+	
+	/////////////-----------END OF GLOBAL------------///////////////
 
-	//Toggle searchbox
-	theHuyApp.avoidNull(desktop_searchToggle);
-	theHuyApp.avoidNull(mobile_headerSidemenuToggle);
 
+	/////////////-----------HEADER------------///////////////
+	
 	//Header mapping
 	theHuyApp.avoidNull(toolsMapping);
 	theHuyApp.avoidNull(customNavMapping);
 	theHuyApp.avoidNull(searchBoxMapping);
 
-	//Footer mapping
-	theHuyApp.avoidNull(footerNavAndInfoSwitch);
-
-	//Back to top button
-	theHuyApp.avoidNull(clickBackToTop);
-
-	//Home Banner Slider
-	theHuyApp.avoidNull(homeBannerInit);
+	//Toggle searchbox
+	theHuyApp.avoidNull(desktop_searchToggle);
+	theHuyApp.avoidNull(mobile_headerSidemenuToggle);
 
 	//Header collapse on scroll listener
 	theHuyApp.avoidNull(collapseHeaderListener);
-})
 
-const desktop_searchToggle = () => {
-	theHuyApp.select('.search-toggle').onclick = () => {
-		theHuyApp.active('.search-wrapper', 'active');
+	/////////////-----------END OF HEADER------------///////////////
+
+
+	/////////////-----------FOOTER------------///////////////
+
+	//Footer mapping
+	theHuyApp.avoidNull(footerNavAndInfoSwitch);
+
+	/////////////-----------END OF FOOTER------------///////////////
+
+
+	/////////////-----------BANNER------------///////////////
+	
+	//Home Banner Slider
+	theHuyApp.avoidNull(homeBannerInit);
+
+	//Other pages banner
+	theHuyApp.avoidNull(normalBannerInit);
+
+	/////////////-----------END OF BANNER------------///////////////
+
+
+	/////////////-----------ABOUT------------///////////////
+
+	//About slider intitialize
+	theHuyApp.avoidNull(aboutSliderInit);
+
+	/////////////-----------END OF ABOUT------------///////////////
+});
+
+/////////////-----------FUNCTIONS------------///////////////
+
+/////////////-----------GLOBAL------------///////////////
+
+const aosInit = () => {
+	AOS.init();
+}
+
+const aosRefreshAfterLoad = () => {
+	window.addEventListener('load', function () {
+		AOS.refresh();
+	});
+}
+
+const clickBackToTop = () => {
+	theHuyApp.select('.backtotop').onclick = () => {
+		theHuyApp.backToTop()
 	}
 }
 
-const mobile_headerSidemenuToggle = () => {
-	theHuyApp.select('.canhcam-header-1 .hamburger').onclick = () => {
-		theHuyApp.active('.canhcam-header-1 .hamburger', 'is-active')
-		theHuyApp.active('.sidemenu-header', 'active')
-		theHuyApp.active('.header-backdrop', 'active')
-	}
+/////////////-----------END OF GLOBAL------------///////////////
 
-	theHuyApp.select('.header-backdrop').onclick = () => {
-		theHuyApp.active('.canhcam-header-1 .hamburger', 'is-active')
-		theHuyApp.active('.sidemenu-header', 'active')
-		theHuyApp.active('.header-backdrop', 'active')
-	}
-}
+
+/////////////-----------HEADER------------///////////////
 
 const toolsMapping = () => {
 	return new MappingListener({
@@ -198,33 +236,28 @@ const searchBoxMapping = () => {
 	}).watch();
 }
 
-const footerNavAndInfoSwitch = () => {
-	return new MappingListener({
-		selector: '.footer-info-wrapper',
-		mobileWrapper: '.footer-nav-wrapper',
-		mobileMethod: 'insertBefore',
-		desktopWrapper: '.footer-nav-wrapper',
-		desktopMethod: 'insertAfter',
-		breakpoint: 992
-	}).watch();
-}
-
-const clickBackToTop = () => {
-	theHuyApp.select('.backtotop').onclick = () => {
-		theHuyApp.backToTop()
+const desktop_searchToggle = () => {
+	theHuyApp.select('.search-toggle').onclick = () => {
+		theHuyApp.active('.search-wrapper', 'active');
 	}
 }
 
-const homeBannerInit = () => {
-	return tns({
-		container: '.banner-home-wrapper',
-		items: 1,
-		slideBy: 'page',
-		autoplay: true,
-		autoplayButtonOutput: false,
-		controls: false,
-		nav: false
-	});
+const mobile_headerSidemenuToggle = () => {
+	theHuyApp.select('.canhcam-header-1 .hamburger').onclick = () => {
+		theHuyApp.active('.canhcam-header-1 .hamburger', 'is-active')
+		theHuyApp.active('.sidemenu-header', 'active')
+		theHuyApp.active('.header-backdrop', 'active')
+	}
+
+	theHuyApp.select('.header-backdrop').onclick = () => {
+		theHuyApp.active('.canhcam-header-1 .hamburger', 'is-active')
+		theHuyApp.active('.sidemenu-header', 'active')
+		theHuyApp.active('.header-backdrop', 'active')
+	}
+}
+
+const setBreadcrumbHomeIcon = () => {
+	theHuyApp.select('.breadcrumb li a span').innerHTML = "<i class='mdi mdi-home'></i>"
 }
 
 const collapseHeaderListener = () => {
@@ -241,12 +274,80 @@ const collapseHeaderListener = () => {
 	});
 }
 
-const aosInit = () => {
-	AOS.init();
+/////////////-----------END OF HEADER------------///////////////
+
+
+/////////////-----------FOOTER------------///////////////
+
+const footerNavAndInfoSwitch = () => {
+	return new MappingListener({
+		selector: '.footer-info-wrapper',
+		mobileWrapper: '.footer-nav-wrapper',
+		mobileMethod: 'insertBefore',
+		desktopWrapper: '.footer-nav-wrapper',
+		desktopMethod: 'insertAfter',
+		breakpoint: 992
+	}).watch();
 }
 
-const aosRefreshAfterLoad = () => {
-	window.addEventListener('load', function () {
-		AOS.refresh();
+/////////////-----------END OF FOOTER------------///////////////
+
+
+/////////////-----------BANNER------------///////////////
+
+const homeBannerInit = () => {
+	return tns({
+		container: '.banner-home-wrapper',
+		items: 1,
+		slideBy: 'page',
+		autoplay: true,
+		autoplayButtonOutput: false,
+		controls: false,
+		nav: false
 	});
 }
+
+const normalBannerInit = () => {
+	return tns({
+		container: '.banner-1-wrapper',
+		items: 1,
+		slideBy: 'page',
+		autoplay: true,
+		autoplayButtonOutput: false,
+		controls: false,
+		nav: false
+	});
+}
+
+
+/////////////-----------END OF BANNER------------///////////////
+
+
+/////////////-----------ABOUT------------///////////////
+
+const aboutSliderInit = () => {
+	return tns({
+		container: '.about-slider',
+		items: 1,
+		slideBy: 'page',
+		autoplay: true,
+		autoplayButtonOutput: false,
+		controls: true,
+		controlsText: ["<span class='mdi mdi-arrow-left'></span>","<span class='mdi mdi-arrow-right'></span>"],
+		nav: false
+	});
+}
+
+/////////////-----------END OF ABOUT------------///////////////
+
+
+
+
+
+
+
+
+
+
+
+/////////////-----------END OF FUNCTIONS------------///////////////
